193ba479aafd96836f7f19d648cf0d27164ccbd2
Nikita Serba <nikitaserba@icloud.com>
2021-05-01 21:24:36 +0300

    Add language field to homograph class

--- src/main/java/me/nikitaserba/rsw/parser/Homograph.java
+++ src/main/java/me/nikitaserba/rsw/parser/Homograph.java
@@ -7,6 +7,7 @@ public class Homograph {
 
     private String word;
     private HomographType type;
+    private String languageCode;
     private boolean isIgnoringDiacritic;  // True if word have homoforms only ignoring diacritical marks
 
 }
