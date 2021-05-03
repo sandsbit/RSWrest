5209f7066966ad7ce5df4a34e82592f5d5b347cc
Nikita Serba <nikitaserba@icloud.com>
2021-05-03 21:28:18 +0300

    Add possible homoforms to word parsing result

--- src/main/java/me/nikitaserba/rsw/parser/repsonses/WordParsingResult.java
+++ src/main/java/me/nikitaserba/rsw/parser/repsonses/WordParsingResult.java
@@ -2,6 +2,8 @@ package me.nikitaserba.rsw.parser.repsonses;
 
 import me.nikitaserba.rsw.parser.HomographParser;
 
+import java.util.Set;
+
 /**
  * Class that stores result of parsing word by `HomographParser`.
  */
@@ -9,11 +11,21 @@ public final class WordParsingResult {
 
     private final String parsedWord;  // original word that was checked
     private final boolean hasHomoforms;
+    private final Set<String> possibleHomoforms;
     private final HomographParser.ParserSettings usedSettings;  // setting that used parser while parsing
 
-    public WordParsingResult(String parsedWord, boolean hasHomoforms, HomographParser.ParserSettings usedSettings) {
+    /**
+     * Create result of parsing word.
+     *
+     * @param parsedWord - word that was parsed.
+     * @param hasHomoforms - if the form have any homoforms depending on used setting.
+     * @param possibleHomoforms - possible homoforms of the word. must be null if `hasHomoforms` is false.
+     * @param usedSettings - settings used while parsing.
+     */
+    public WordParsingResult(String parsedWord, boolean hasHomoforms, Set<String> possibleHomoforms, HomographParser.ParserSettings usedSettings) {
         this.parsedWord = parsedWord;
         this.hasHomoforms = hasHomoforms;
+        this.possibleHomoforms = possibleHomoforms;
         this.usedSettings = usedSettings;
     }
 
@@ -23,6 +35,10 @@ public final class WordParsingResult {
 
     public boolean isHavingHomoforms() {
         return hasHomoforms;
+    }
+
+    public Set<String> getPossibleHomoforms() {
+        return possibleHomoforms;
     }
 
     public HomographParser.ParserSettings getUsedSettings() {
