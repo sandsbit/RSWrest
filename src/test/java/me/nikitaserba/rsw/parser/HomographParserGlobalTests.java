b93222478ad0c0bd57d0bb57740d5fcebdc9586a
Nikita Serba <nikitaserba@icloud.com>
2021-05-03 21:05:09 +0300

    Add tests for loading homoforms dictionaries

---
+++ src/test/java/me/nikitaserba/rsw/parser/HomographParserGlobalTests.java
@@ -0,0 +1,25 @@
+package me.nikitaserba.rsw.parser;
+
+import manifold.ext.rt.api.Jailbreak;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HomographParserGlobalTests {
+
+    @Jailbreak HomographParser parser = null;
+
+    @Test
+    @DisplayName("Test that language utils return number of languages properly")
+    void TestLoadingDictionary() {
+        Map<String, List<Homograph>> dictionaries = parser.homographs;
+        assertEquals(1, dictionaries.size());
+        assertTrue(dictionaries.containsKey("ru-RU"));
+        assertEquals(2, dictionaries.get("ru-RU").size());
+    }
+
+}
