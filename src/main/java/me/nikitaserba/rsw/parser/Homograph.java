8ce676843d6fd1505e93341034dd1b488114351f
Nikita Serba <nikitaserba@icloud.com>
2021-05-03 20:41:54 +0300

    Fix Homograph class structure

--- src/main/java/me/nikitaserba/rsw/parser/Homograph.java
+++ src/main/java/me/nikitaserba/rsw/parser/Homograph.java
@@ -1,13 +1,37 @@
 package me.nikitaserba.rsw.parser;
 
+import java.util.Set;
+
 /**
  * Class that represents homograph parsed from dictionary.
  */
 public final class Homograph {
 
-    private String word;
-    private HomographType type;
-    private String languageCode;
-    private boolean isIgnoringDiacritic;  // True if word have homoforms only ignoring diacritical marks
+    private final String word;
+    private final Set<String> homoforms;
+    private final HomographType type;
+    private final boolean isIgnoringDiacritic;  // True if word have homoforms only ignoring diacritical marks
 
+    public Homograph(String word, Set<String> homoforms, HomographType type, boolean isIgnoringDiacritic) {
+        this.word = word;
+        this.homoforms = homoforms;
+        this.type = type;
+        this.isIgnoringDiacritic = isIgnoringDiacritic;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public Set<String> getHomoforms() {
+        return homoforms;
+    }
+
+    public HomographType getType() {
+        return type;
+    }
+
+    public boolean isIgnoringDiacritic() {
+        return isIgnoringDiacritic;
+    }
 }
