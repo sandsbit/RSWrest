5a43eb685a299d335cfbd7eeb9191ca455d4c637
Nikita Serba <nikitaserba@icloud.com>
2021-05-03 21:29:21 +0300

    Change description of PROPER_NAME type of homograph

--- src/main/java/me/nikitaserba/rsw/parser/HomographType.java
+++ src/main/java/me/nikitaserba/rsw/parser/HomographType.java
@@ -8,6 +8,7 @@ public enum HomographType {
     ONE_PART_OF_SPEECH,  // different homoforms of the word are the same part of speech
     DIFFERENT_PART_OF_SPEECH, // different homoforms of the word are different part of speech
     FORMS_OF_ONE_WORD, // different homoforms of the word are forms of the one particular word,
-    PROPER_NAME // some homoforms of the world are proper names, while others are not
+    // word may be understood incorrectly due to existence of homoforms ONLY if it may be a proper name (starts with capital letter)
+    PROPER_NAME
 
 }
