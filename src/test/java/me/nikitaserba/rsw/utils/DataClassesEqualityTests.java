c9d5dc7a3c1c65d07ba4a37a98fc5607ec041b41
Nikita Serba <nikitaserba@icloud.com>
2021-05-06 19:51:43 +0300

    Test data classes used in `utils` package

---
+++ src/test/java/me/nikitaserba/rsw/utils/DataClassesEqualityTests.java
@@ -0,0 +1,24 @@
+package me.nikitaserba.rsw.utils;
+
+import me.nikitaserba.rsw.utils.dataclasses.Language;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class DataClassesEqualityTests {
+    @Test
+    @DisplayName("Test equal() and hashCode() of Language class")
+    void TestLanguageClass() {
+        Language eq1 = new Language("Russian", "ru-RU");
+        Language eq2 = new Language("Russian (Russia)", "ru-RU");
+
+        Language diff1 = new Language("Russian", "ru-ES");
+
+        assert eq1 != eq2;
+        assertEquals(eq1, eq2);
+
+        assertNotEquals(eq1, diff1);
+    }
+}
