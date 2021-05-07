c6fe92ecf31f2b09ab7e660887a6662795638e31
Nikita Serba <nikitaserba@icloud.com>
2021-05-07 22:27:17 +0300

    Add about response class

---
+++ src/main/java/me/nikitaserba/rsw/rest/controllers/repsonses/AboutResponse.java
@@ -0,0 +1,26 @@
+package me.nikitaserba.rsw.rest.controllers.repsonses;
+
+import com.google.gson.annotations.SerializedName;
+
+public final class AboutResponse {
+
+    @SerializedName("service_name")
+    private final String serviceName = "RSW rest-api";
+    private final String version = "v1.0";
+    @SerializedName("version_code")
+    private final int versionCode = 0;
+
+    public AboutResponse() {}
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public int getVersionCode() {
+        return versionCode;
+    }
+}
