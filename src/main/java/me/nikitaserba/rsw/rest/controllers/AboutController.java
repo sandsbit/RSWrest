da314f3bd3b97feb846e2369f39cd0aa3dbc7c69
Nikita Serba <nikitaserba@icloud.com>
2021-05-07 23:14:31 +0300

    Add javadocs

--- src/main/java/me/nikitaserba/rsw/rest/controllers/AboutController.java
+++ src/main/java/me/nikitaserba/rsw/rest/controllers/AboutController.java
@@ -1,14 +1,19 @@
 package me.nikitaserba.rsw.rest.controllers;
 
 import me.nikitaserba.rsw.rest.repsonses.AboutResponse;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
 
 @RequestMapping("/about")
 @RestController
 public final class AboutController {
 
+    /**
+     * Returns general information about service.
+     */
     @GetMapping
     public AboutResponse about() {
         return new AboutResponse();
