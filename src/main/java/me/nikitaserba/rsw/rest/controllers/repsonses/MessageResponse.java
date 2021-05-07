f15ce21b3d1e2984e1c9da94d6e034f63a75f696
Nikita Serba <nikitaserba@icloud.com>
2021-05-07 22:32:10 +0300

    Add message response

---
+++ src/main/java/me/nikitaserba/rsw/rest/controllers/repsonses/MessageResponse.java
@@ -0,0 +1,23 @@
+package me.nikitaserba.rsw.rest.controllers.repsonses;
+
+import com.google.gson.annotations.SerializedName;
+
+public final class MessageResponse {
+
+    @SerializedName("response_code")
+    private final int responseCode;
+    private final String message;
+
+    public MessageResponse(int responseCode, String message) {
+        this.responseCode = responseCode;
+        this.message = message;
+    }
+
+    public int getResponseCode() {
+        return responseCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
