88df11b8271619676502a38b1d2f72e66ea498b0
Nikita Serba <nikitaserba@icloud.com>
2021-05-07 23:48:19 +0300

    Automate right naming in json responses

--- src/main/java/me/nikitaserba/rsw/rest/repsonses/MessageResponse.java
+++ src/main/java/me/nikitaserba/rsw/rest/repsonses/MessageResponse.java
@@ -1,23 +1,12 @@
 package me.nikitaserba.rsw.rest.repsonses;
 
-import com.google.gson.annotations.SerializedName;
-
 public final class MessageResponse {
 
-    @SerializedName("response_code")
     private final int responseCode;
     private final String message;
 
     public MessageResponse(int responseCode, String message) {
         this.responseCode = responseCode;
         this.message = message;
-    }
-
-    public int getResponseCode() {
-        return responseCode;
-    }
-
-    public String getMessage() {
-        return message;
     }
 }
