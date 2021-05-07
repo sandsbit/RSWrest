12fcd649d3274f70c19faa1a546efa2455749cde
Nikita Serba <nikitaserba@icloud.com>
2021-05-07 15:04:12 +0300

    Javadocs in Session class

--- src/main/java/me/nikitaserba/rsw/parser/repsonses/Session.java
+++ src/main/java/me/nikitaserba/rsw/parser/repsonses/Session.java
@@ -12,7 +12,10 @@ public final class Session {
 
     // STATIC PART (Storing sessions)
 
+    // Time that session exist while it is not accessed.
+    // It is NOT guaranteed that session will be removed at that time, but it MIGHT.
     private static final Duration SESSION_EXPIRE = Duration.ofMinutes(5);
+    // timeout for threads that removes expired sessions
     private static final Duration RECHECK_EXPIRE_EVERY = Duration.ofMinutes(5);
 
     private static ConcurrentMap<String, Session> sessions = new ConcurrentHashMap<>();
@@ -20,6 +23,14 @@ public final class Session {
 
     private static ScheduledExecutorService expiredSessionsCleaner;
 
+    /**
+     * Starts thread that removes sessions that have expired.
+     *
+     * That thread WILL NOT stop after closing the application, so you MUST call `stopExpiredSessionsCleaner()` before
+     * finishing application.
+     *
+     * @throws IllegalStateException if cleaner is already started.
+     */
     public static void startExpiredSessionsCleaner() {
         if (expiredSessionsCleaner != null)
             throw new IllegalStateException("Expired sessions cleaner is already started.");
@@ -36,6 +47,15 @@ public final class Session {
         }, 0, RECHECK_EXPIRE_EVERY.toSeconds(), TimeUnit.SECONDS);
     }
 
+    /**
+     * Stops thread that cleans expired sessions.
+     *
+     * Thread must be started with `startExpiredSessionsCleaner` first. In almost all cases this method must be called
+     * before finishing program.
+     *
+     * @throws InterruptedException if interrupted while waiting.
+     * @throws IllegalStateException if cleaner is not running.
+     */
     public static void stopExpiredSessionsCleaner() throws InterruptedException {
         if (expiredSessionsCleaner == null)
             throw new IllegalStateException("Expire sessions cleaner isn't running");
@@ -45,6 +65,13 @@ public final class Session {
             expiredSessionsCleaner.shutdownNow();
     }
 
+    /**
+     * Creates new session.
+     *
+     * Token of new Session is saved in Session class.
+     *
+     * @return new Instance of Session class.
+     */
     public static Session create() {
         String token;
         do {
@@ -57,11 +84,23 @@ public final class Session {
         return newSession;
     }
 
+    /**
+     * Deletes session by its token.
+     *
+     * @param token - token of session that must be deleted.
+     * @return true if it was deleted, false if there is no sessions associated with given token.
+     */
     public static boolean delete(String token) {
         sessionLastAccessed.remove(token);
         return sessions.remove(token) == null;
     }
 
+    /**
+     * Returns Session class by token.
+     *
+     * @param token - token to search for.
+     * @return Session class that has the given token or null if there's no sessions with that token.
+     */
     public static Session getByToken(String token) {
         sessionLastAccessed.put(token, LocalDateTime.now());
         return sessions.get(token);
@@ -69,14 +108,15 @@ public final class Session {
 
     // GENERAL PART (Session itself)
 
-    private final String token;
+    private final String token;  // it will be used by client to identify session
 
-    private String text;
-    private TextParsingResult textParsingResultCache;
+    private String text;  // last text that was parsed in that session
+    private TextParsingResult textParsingResultCache; // result of parsing `text`
 
-    private String word;
-    private Map<String, WordParsingResult> wordParsingResultCache;
+    private String word;  // last word that was parsed in that session
+    private Map<String, WordParsingResult> wordParsingResultCache;  // cache of parsing words in that session
 
+    // it is private so session can't be created not in `create` method
     private Session(String token) {
         this.token = token;
         wordParsingResultCache = new HashMap<>();
