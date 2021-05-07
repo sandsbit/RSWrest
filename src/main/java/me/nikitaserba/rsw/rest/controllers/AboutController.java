f02cfe04c968e41b36a283327b5fdd638a80547a
Nikita Serba <nikitaserba@icloud.com>
2021-05-07 22:27:39 +0300

    Simple rest))) + using gson in spring

---
+++ src/main/java/me/nikitaserba/rsw/rest/controllers/AboutController.java
@@ -0,0 +1,17 @@
+package me.nikitaserba.rsw.rest.controllers;
+
+import me.nikitaserba.rsw.rest.controllers.repsonses.AboutResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/about")
+@RestController
+public final class AboutController {
+
+    @GetMapping
+    public AboutResponse about() {
+        return new AboutResponse();
+    }
+
+}
