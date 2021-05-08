e0864b54e6ae6bc35250b48fb03cdc46892c2ef9
Nikita Serba <nikitaserba@icloud.com>
2021-05-09 00:29:32 +0300

    Add Gson config

---
+++ src/main/java/me/nikitaserba/rsw/rest/configs/GsonConfig.java
@@ -0,0 +1,24 @@
+package me.nikitaserba.rsw.rest.configs;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import me.nikitaserba.rsw.parser.ParserSettings;
+import me.nikitaserba.rsw.parser.ParserSettingsDeserializer;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnClass(Gson.class)
+public class GsonConfig {
+    @Bean
+    public Gson gson() {
+        return new GsonBuilder()
+                .registerTypeAdapter(ParserSettings.class, new ParserSettingsDeserializer())
+                .setPrettyPrinting()
+                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                .serializeNulls()
+                .create();
+    }
+}
